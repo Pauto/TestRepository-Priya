8f115447fb3ed1ba1be62d0ad2c016b94cc836b4
PriyaBarmi <barmip@gmail.com>
2015-12-25 09:47:42 -0800

    Exercise 1 & 2

---
+++ AddNumber.java
@@ -0,0 +1,14 @@
+public class AddNumber
+{
+	public static void main(String[] args)
+	{
+		//DataType variables;
+		int i=10,j=20;
+		int sum;
+		
+
+		//output of two variables
+		sum = i + j;
+		System.out.println("Sum of two Variables: "+sum); 
+	}
+}
---
+++ SwapNumber.java
@@ -0,0 +1,14 @@
+public class SwapNumber
+{
+	public static void main(String[] args)
+	{
+		//DataType variables;
+		int A=25,B=35;
+		System.out.println("Before Swap A:"+A+" , B:"+B);
+
+		//output of two variables after swap
+		A=35;
+		B=25;
+		System.out.println("After Swap A:"+A+" , B:"+B); 
+	}
+}
